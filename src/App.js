a19d4382f7efff9b95ab0db9d65321616db28e95
tarique313 <mahmud.tarique@gmail.com>
2018-11-23 00:56:32 +0600

    Added Contact and Header component with bootstrap styled.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="Gazi Tarique Mahmud"
+            email="mahmud@email.com"
+            phone="014444444" />
+          <Contact name="Asif Mahmud"
+            email="asif@email.com"
+            phone="013333333" />
+        </div>
       </div>
     );
   }
